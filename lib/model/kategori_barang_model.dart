1e6bf8fad9a5f0f271b3bef0a60e3c0c37f4450d
cihuyyama <miqbalalhabib@gmail.com>
2023-05-14 18:44:34 +0700

    nambahin id di model

--- lib/model/kategori_barang_model.dart
+++ lib/model/kategori_barang_model.dart
@@ -2,27 +2,33 @@ import 'dart:convert';
 
 class KategoriBarangModel {
   final String nama;
+  var id;
   KategoriBarangModel({
     required this.nama,
+    this.id,
   });
 
   KategoriBarangModel copyWith({
     String? nama,
+    int? id,
   }) {
     return KategoriBarangModel(
       nama: nama ?? this.nama,
+      id: id ?? this.id,
     );
   }
 
   Map<String, dynamic> toMap() {
     return {
       'nama_kategori_barang': nama,
+      'id_kategori_barang': id.toMap(),
     };
   }
 
   factory KategoriBarangModel.fromMap(Map<String, dynamic> map) {
     return KategoriBarangModel(
       nama: map['nama_kategori_barang'] ?? '',
+      id: map['id_kategori_barang'] ?? '',
     );
   }
 
@@ -32,15 +38,15 @@ class KategoriBarangModel {
       KategoriBarangModel.fromMap(json.decode(source));
 
   @override
-  String toString() => 'KategoriBarangModel(nama: $nama)';
+  String toString() => 'KategoriBarangModel(nama: $nama, id: $id)';
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
 
-    return other is KategoriBarangModel && other.nama == nama;
+    return other is KategoriBarangModel && other.nama == nama && other.id == id;
   }
 
   @override
-  int get hashCode => nama.hashCode;
+  int get hashCode => nama.hashCode ^ id.hashCode;
 }
