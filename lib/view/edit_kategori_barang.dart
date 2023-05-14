822d4a46a06ea97d369bdc2757a6d0018452b9c6
cihuyyama <miqbalalhabib@gmail.com>
2023-05-14 19:46:54 +0700

    bikin logic di edit kategori barang

--- lib/view/edit_kategori_barang.dart
+++ lib/view/edit_kategori_barang.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+import 'package:materiapi/controller/kategori_barang_controller.dart';
+import 'package:materiapi/model/kategori_barang_model.dart';
+import 'package:materiapi/view/kategori_barang.dart';
 
 class EditKategoriBarang extends StatefulWidget {
   const EditKategoriBarang({super.key});
@@ -9,8 +11,62 @@ class EditKategoriBarang extends StatefulWidget {
 }
 
 class _EditKategoriBarangState extends State<EditKategoriBarang> {
+  final kategoriBarangController = KategoriBarangController();
+  String? nama;
+  int? id;
+
+  void editKategoriBarang() async{
+    KategoriBarangModel kategoriBarang = KategoriBarangModel(nama: nama!, id: id);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var formkey = GlobalKey<FormState>();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tambah Kategori Barang'),
+      ),
+      body: Form(
+        key: formkey,
+        child: ListView(
+          padding: const EdgeInsets.all(16.0),
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: 'Nama Kategori Barang',
+                labelText: 'Nama Kategori Barang',
+              ),
+              onChanged: (value) {
+                nama = value;
+              },
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Nama Kategori is required';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                if (formkey.currentState!.validate()) {
+                  formkey.currentState!.save();
+                  editKategoriBarang();
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const KategoriBarang()));
+
+                  var snackBar =
+                      const SnackBar(content: Text('Data Berhasil Disimpan'));
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                }
+              },
+              child: const Text('Simpan'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
